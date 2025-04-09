312f59f88e8ef6533ceac67e12d2eae58e15acb8
gabriellaxavier05 <gabriellaxaviersp05@outlook.com>
2025-04-09 18:44:17 -0300

    Correção removendo redundância exerc. boas_vindas

--- Sintaxe básica/boas_vindas/script.js
+++ Sintaxe básica/boas_vindas/script.js
@@ -1,6 +1,3 @@
-btnEnviar.addEventListener("click", boasVindas); // Adiciona um evento de clique ao botão "Enviar"
-btnLimpar.addEventListener("click", limpar); // Adiciona um evento de clique ao botão "Limpar"
-
 // Função que exibe um alerta de boas-vindas
 function boasVindas() {
     window.alert("Bem-vindo(a) à página!");
